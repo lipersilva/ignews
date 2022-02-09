cb5d15f8800503a38cd0fb2fe6655b13e8d8f26a
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-02-09 18:57:52 -0300

    FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth" //verificar se é mesmo /react
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
+import { query as q} from 'faunadb'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,4 +18,44 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  // jwt: {
+  //   signingKey: process.env.SIGNING_KEY,
+  // },
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // console.log(user);
+      // return true
+      const { email } = user;
+      
+      try{
+        await fauna.query(
+          q.If (
+            q.Not (
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
+
 })
