bbefb132f46e37daee7a7e5db25f1d3b5e82af1f
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-02-10 13:32:09 -0300

    payments - webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 
@@ -5,10 +8,30 @@ interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton ({priceId } : SubscribeButtonProps) {
+  const {data: session } = useSession();
+  
+  async function handleSubscribe(){
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    //criação da checkout session
+    try{
+      const response = await api.post('/subscribe')
+      const  { sessionId } = response.data;
+      const stripe = await getStripeJS()
+      await stripe.redirectToCheckout({sessionId})
+
+    } catch (err){
+      alert(err.message);      
+    }
+  }
   return(
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
