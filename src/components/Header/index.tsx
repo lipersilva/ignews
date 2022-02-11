e51f71d8f0822c8a74e5a327776d3536822126a5
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-02-11 11:47:02 -0300

    Prismic - CMS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,27 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
+
 
 export function Header (){
+  
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
+
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            {/* condicional para mostrar o link ativo ( barrinha amarela ) */}
+            {/* <a  className={asPath === '/' ? styles.active : ''}>Home</a> */}
+            <a>Home</a> 
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            {/* <a className={asPath === '/post' ? styles.active : ''}>Posts</a> */}
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
+
         <SignInButton/>
       </div>
     </header>
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,70 @@
+import styles from './styles.module.scss'
+import Head from 'next/head'
+import Prismic from '@prismicio/client'
+import { getPrismicClient } from '../../services/prismic'
+import {GetStaticProps} from 'next';
+import {RichText} from 'prismic-dom';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps{
+  posts: Post[]
+}
+export default function Posts({ posts } : PostsProps) {
+  return (
+    <>
+      <Head>
+        <title> Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          { posts.map(post => (
+            <a key={post.slug} href="#"> 
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>        
+          )) }
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type','posts')
+  ], {
+    fetch: ['posts.title','posts.content'],
+    pageSize: 100,
+  })
+  
+  const posts = response.results.map(post => {
+    return {
+      slugs: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type ==='paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+
+    };
+  });
+  //console.log(response) //debugger
+  //console.log(JSON.stringify(response,null,2)) //debugger para visualizar valores dentro de []
+  return {
+    props: {
+      posts
+    }
+  }
+}
